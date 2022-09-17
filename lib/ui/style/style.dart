6516a9ffa1d67d46cf07dc2e4a72012efffafa4a
Subamurugan001 <subamurugan2612@gmail.com>
2022-09-17 10:19:42 +0530

    done

---
+++ lib/ui/style/style.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppStyle{
+  static Color primaryColor = Colors.red;
+  static Color textInputColor = Colors.blue;
+  static Color accentColor = Colors.green;
+  
+}
